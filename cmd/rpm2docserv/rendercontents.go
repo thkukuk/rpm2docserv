e17c24cb0448045007d39dd1dd1ff0742eab7462
Thorsten Kukuk <kukuk@suse.com>
2025-03-07 15:36:27 +0100

    Unify template.Execute calls/variables

--- cmd/rpm2docserv/rendercontents.go
+++ cmd/rpm2docserv/rendercontents.go
@@ -3,11 +3,8 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"io"
 
 	"github.com/thkukuk/rpm2docserv/pkg/bundled"
-	"github.com/thkukuk/rpm2docserv/pkg/manpage"
-	"github.com/thkukuk/rpm2docserv/pkg/write"
 )
 
 var contentsTmpl = mustParseContentsTmpl()
@@ -17,37 +14,14 @@ func mustParseContentsTmpl() *template.Template {
 }
 
 func renderProductContents(dest, productName string, pkgdirs []string, srcpkgdirs []string, gv globalView) error {
-	if err := write.Atomically(dest, false, func(w io.Writer) error {
-		return contentsTmpl.Execute(w, struct {
-			Title          string
-			ProjectName    string
-			ProjectUrl     string
-			LogoUrl        string
-			Rpm2docservVersion string
-			IsOffline      bool
-			Breadcrumbs    breadcrumbs
-			FooterExtra    string
-			ProductName    string
-			PkgDirs        []string
-			SrcPkgDirs     []string
-			Products       []string
-			Meta           *manpage.Meta
-			HrefLangs      []*manpage.Meta
-		}{
-			Title:          fmt.Sprintf("Manpages of %s", productName),
-			ProjectName:    projectName,
-			ProjectUrl:     projectUrl,
-			LogoUrl:        logoUrl,
-			IsOffline:      isOffline,
-			Rpm2docservVersion: rpm2docservVersion,
-			Breadcrumbs: breadcrumbs{
-				{"", productName},
-			},
-			PkgDirs:        pkgdirs,
-			SrcPkgDirs:     srcpkgdirs,
-			ProductName:    productName,
-		        Products:       productList,
-		})
+	if err := renderExec(dest, contentsTmpl, tmplData {
+		Title:          fmt.Sprintf("Manpages of %s", productName),
+		Breadcrumbs: breadcrumbs{
+			{"", productName},
+		},
+		PkgDirs:        pkgdirs,
+		SrcPkgDirs:     srcpkgdirs,
+		ProductName:    productName,
 	}); err != nil {
 		return err
 	}
---
+++ cmd/rpm2docserv/renderexec.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/thkukuk/rpm2docserv/pkg/manpage"
+	"github.com/thkukuk/rpm2docserv/pkg/write"
+)
+
+type tmplData struct {
+	// the following variables are set by us
+	ProjectName        string
+	ProjectUrl         string
+	LogoUrl            string
+	IsOffline          bool
+	Rpm2docservVersion string
+	Products           []string
+	// the following variables needs to be set by the caller
+	Title              string
+	Breadcrumbs        breadcrumbs
+	FooterExtra        template.HTML
+	ProductName        string
+	Meta               *manpage.Meta
+	HrefLangs          []*manpage.Meta
+	PkgDirs            []string
+	SrcPkgDirs         []string
+}
+
+func renderExec(dest string, tmpl *template.Template, data tmplData) error {
+
+	data.ProjectName = projectName
+	data.ProjectUrl = projectUrl
+	data.LogoUrl = logoUrl
+	data.IsOffline = isOffline
+	data.Rpm2docservVersion = rpm2docservVersion
+	data.Products = productList
+
+        return write.Atomically(dest, false, func(w io.Writer) error {
+                return tmpl.Execute(w, data)
+        })
+}
